447abeaf176dea7509a1ef291724df65b0edfed4
Mustafizur Rahaman <61288061+MustfizurRahaman@users.noreply.github.com>
2024-01-30 21:19:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-html, body {
-    height: 100%;
-    width: 100%;
-    margin: 0;
-    padding: 0;
-}
-#viewDiv {
-    height: 100%;
-    width: 100%;
-    background-color: green; /* You can remove this if you don't want a green background */
-}
+
